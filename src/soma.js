228eb3bdd2a5e1c0bff397b2b62d99cb31b02c26
edutbx <eduardo.abreu@fatec.sp.gov.br>
2026-03-25 08:51:15 -0300

    quinta tentativa sonar

--- src/soma.js
+++ src/soma.js
@@ -1,5 +1,12 @@
 function soma(a, b) { 
 eval("console.log('teste')"); // linha problemática 
-// return a + b; 
+return a + b; 
 } 
 module.exports = soma;
+
+
+// function soma(a, b) { 
+// eval("console.log('teste')"); // linha problemática 
+// return a + b; 
+// } 
+// module.exports = soma;
